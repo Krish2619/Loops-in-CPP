cf97c743618369b31b31770abbb6a842d9f0291b
Krish2619 <krishna.chhabaria.btech2024@sitpune.edu.in>
2025-07-29 14:55:58 +0530

    Add files via upload

---
+++ EXP-06b.cpp
@@ -0,0 +1,20 @@
+//Krishna Chhabaria
+//24070123149
+//B3
+#include <iostream>
+using namespace std;
+int main (){
+    for (int i=0;i<5;i++){
+       
+            cout<<i+1<<"."<< "Symbiosis"<<endl;
+       
+    }
+  return 0;
+}
+
+//output
+//1.Symbiosis
+//2.Symbiosis
+//3.Symbiosis
+//4.Symbiosis
+//5.Symbiosis
---
+++ EXP-06j.cpp
@@ -0,0 +1,39 @@
+//Krishna Chhabaria
+//24070123149
+//B3
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string pass = "hi123";
+    string in;
+   
+    cout << "Enter the password: ";
+    cin >> in;
+
+    if (in.size() == pass.size())
+    {
+        for (int i = 0; i < pass.size(); i++)
+        {
+            if (pass[i] != in[i])
+            {
+                cout << "The password is wrong";
+            return 0;
+            }
+       
+            else
+            {
+                cout << "The password is correct";
+                return 0;
+            }
+        }
+    }
+    
+    else
+    {
+        cout << "The password is wrong";
+        return 0;
+    }
+}
